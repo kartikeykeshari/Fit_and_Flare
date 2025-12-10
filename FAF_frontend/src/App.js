94fe093bd8bcaadcae62814084fc1ee05bb946e3
Kartikey Keshari <kesharikartikey9140@gmail.com>
2025-12-10 11:45:33 +0530

    Changes in App.jsx file

--- FAF_frontend/src/App.js
+++ FAF_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import VerifyOtp from "./pages/VerifyOtp";
@@ -9,6 +9,8 @@ export default function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+
           <Route path="/register" element={<Register />} />
           <Route path="/verify-otp" element={<VerifyOtp />} />
           <Route path="/login" element={<Login />} />
